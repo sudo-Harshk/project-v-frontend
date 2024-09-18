ddd0ba64cb5130571d4d477cd20d27a7cbe5569b
sudo-Harshk <harshk1744@gmail.com>
2024-09-18 06:01:23 +0530

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -320,3 +320,79 @@ document.addEventListener('DOMContentLoaded', () => {
     jobsSection.style.display = 'block';
   });
 });
+
+
+
+// video animation 
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomLetter() {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return alphabet[rand(0, alphabet.length - 1)];
+}
+
+function getRandomWord(word) {
+  var text = word.innerHTML;
+  var finalWord = '';
+  for (var i = 0; i < text.length; i++) {
+      finalWord += text[i] === ' ' ? ' ' : getRandomLetter();
+  }
+  return finalWord;
+}
+
+var word = document.querySelector('#video-search'); 
+var interv;
+var canChange = false;
+var globalCount = 0;
+var count = 0;
+var INITIAL_WORD = word.innerHTML;
+var isGoing = false;
+
+function init() {
+  if (isGoing) return;
+
+  isGoing = true;
+  var randomWord = getRandomWord(word);
+  word.innerHTML = randomWord;
+
+  interv = setInterval(function() {
+      var finalWord = '';
+      for (var x = 0; x < INITIAL_WORD.length; x++) {
+          if (x <= count && canChange) {
+              finalWord += INITIAL_WORD[x];
+          } else {
+              finalWord += getRandomLetter();
+          }
+      }
+      word.innerHTML = finalWord;
+      if (canChange) {
+          count++;
+      }
+      if (globalCount >= 40) { // Adjusted for slower animation
+          canChange = true;
+      }
+      if (count >= INITIAL_WORD.length) {
+          clearInterval(interv);
+          count = 0;
+          canChange = false;
+          globalCount = 0;
+          isGoing = false;
+      }
+      globalCount++;
+  }, 100); // Adjusted for slower animation
+}
+
+function startAnimation() {
+  if (!isGoing) {
+      init();
+  }
+  setInterval(function() {
+      if (!isGoing) {
+          init();
+      }
+  }, 10000); 
+}
+
+startAnimation();
